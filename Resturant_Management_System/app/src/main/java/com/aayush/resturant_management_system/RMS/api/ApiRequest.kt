10c233fb2b61acb36c63e42b7fc313049192de43
AKAaayush <shresthaaayush81@gmail.com>
2021-02-22 20:04:08 +0545

    API Integation

--- Resturant_Management_System/app/src/main/java/com/aayush/resturant_management_system/RMS/api/ApiRequest.kt
+++ Resturant_Management_System/app/src/main/java/com/aayush/resturant_management_system/RMS/api/ApiRequest.kt
@@ -5,8 +5,7 @@ import org.json.JSONObject
 import retrofit2.Response
 import java.io.IOException
 import java.lang.StringBuilder
-
-open class ApiRequest {
+abstract class ApiRequest {
     suspend fun<T: Any>apiRequest(call: suspend() -> Response<T>): T{
         val response= call.invoke()
         if(response.isSuccessful){
